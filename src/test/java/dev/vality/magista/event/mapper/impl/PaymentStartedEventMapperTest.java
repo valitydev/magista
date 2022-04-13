67061cbff3454ce46b48bb141c8e4d2c6cba34dc
Inal Arsanukaev <inalarsanukaev@192.168.1.5>
2022-04-13 13:56:27 +0300

    Added test

--- src/test/java/dev/vality/magista/event/mapper/impl/PaymentStartedEventMapperTest.java
+++ src/test/java/dev/vality/magista/event/mapper/impl/PaymentStartedEventMapperTest.java
@@ -7,6 +7,7 @@ import dev.vality.damsel.payment_processing.InvoicePaymentChangePayload;
 import dev.vality.damsel.payment_processing.InvoicePaymentStarted;
 import dev.vality.machinegun.eventsink.MachineEvent;
 import dev.vality.magista.domain.tables.pojos.PaymentData;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -14,10 +15,26 @@ import static org.junit.jupiter.api.Assertions.*;
 public class PaymentStartedEventMapperTest {
 
     @Test
-    public void testMap() {
+    public void testMapPaymentTerminal() {
         var mapper = new PaymentStartedEventMapper();
         String paymentServiceId = "paymentServiceId";
-        InvoiceChange change = InvoiceChange.invoice_payment_change(
+        InvoiceChange change = buildChange(buildPaymentToolTerminal(paymentServiceId));
+        PaymentData paymentData = mapper.map(change, new MachineEvent().setCreatedAt("2016-03-22T06:12:27Z"));
+        assertEquals(paymentServiceId, paymentData.getPaymentTerminalPaymentServiceRefId());
+    }
+
+    @Test
+    public void testMapDigitalWallet() {
+        var mapper = new PaymentStartedEventMapper();
+        String paymentServiceId = "paymentServiceId";
+        InvoiceChange change = buildChange(buildPaymentToolWallet(paymentServiceId));
+        PaymentData paymentData = mapper.map(change, new MachineEvent().setCreatedAt("2016-03-22T06:12:27Z"));
+        assertEquals(paymentServiceId, paymentData.getPaymentDigitalWalletServiceRefId());
+    }
+
+    @NotNull
+    private InvoiceChange buildChange(PaymentTool paymentTool) {
+        return InvoiceChange.invoice_payment_change(
                 new InvoicePaymentChange()
                         .setId("124")
                         .setPayload(InvoicePaymentChangePayload.invoice_payment_started(
@@ -25,17 +42,26 @@ public class PaymentStartedEventMapperTest {
                                         .setCost(new Cash(112L, new CurrencyRef()))
                                         .setCreatedAt("2016-03-22T06:12:27Z")
                                         .setFlow(InvoicePaymentFlow.instant(new InvoicePaymentFlowInstant()))
-                                                .setStatus(InvoicePaymentStatus.pending(new InvoicePaymentPending()))
+                                        .setStatus(InvoicePaymentStatus.pending(new InvoicePaymentPending()))
                                         .setPayer(Payer.payment_resource(new PaymentResourcePayer()
                                                 .setContactInfo(new ContactInfo())
-                                                .setResource(
-                                                        new DisposablePaymentResource().setPaymentTool(
-                                                                PaymentTool.payment_terminal(new PaymentTerminal()
-                                                                        .setPaymentService(new PaymentServiceRef()
-                                                                                .setId(paymentServiceId)))))
+                                                .setResource(new DisposablePaymentResource()
+                                                        .setPaymentTool(paymentTool))
                                         )))
                         )));
-        PaymentData paymentData = mapper.map(change, new MachineEvent().setCreatedAt("2016-03-22T06:12:27Z"));
-        assertEquals(paymentServiceId, paymentData.getPaymentTerminalPaymentServiceRefId());
+    }
+
+    @NotNull
+    private PaymentTool buildPaymentToolTerminal(String paymentServiceId) {
+        return PaymentTool.payment_terminal(new PaymentTerminal()
+                .setPaymentService(new PaymentServiceRef()
+                        .setId(paymentServiceId)));
+    }
+
+    @NotNull
+    private PaymentTool buildPaymentToolWallet(String paymentServiceId) {
+        return PaymentTool.digital_wallet(new DigitalWallet()
+                .setPaymentService(new PaymentServiceRef()
+                        .setId(paymentServiceId)));
     }
 }
