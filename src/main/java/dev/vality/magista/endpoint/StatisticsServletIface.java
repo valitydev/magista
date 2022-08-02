f48da55051c976887a559403bf0ef7c987330887
Anatoly Karlov <karleowne@gmail.com>
2022-08-02 18:02:56 +0700

    TD-358: remove merch_stat deprecated handler

--- src/main/java/dev/vality/magista/endpoint/StatisticsServletIface.java
+++
@@ -1,7 +0,0 @@
-package dev.vality.magista.endpoint;
-
-import dev.vality.damsel.merch_stat.DarkMessiahStatisticsSrv;
-import dev.vality.damsel.merch_stat.MerchantStatisticsSrv;
-
-public interface StatisticsServletIface extends MerchantStatisticsSrv.Iface, DarkMessiahStatisticsSrv.Iface {
-}
