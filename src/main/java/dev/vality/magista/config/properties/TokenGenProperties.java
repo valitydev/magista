55b6e33137803dd9593f6ee108ae44db27394984
Gretchen-z <68681408+Gretchen-z@users.noreply.github.com>
2024-06-13 16:11:07 +0300

    Ft/td 921 bump to SB3, JDK21 (#57)
    * move to Spring Boot 2 and Jdk 21
    
    * change kafka error handler and bump libs
    
    * exclusion commons-logging lib

--- src/main/java/dev/vality/magista/config/properties/TokenGenProperties.java
+++ src/main/java/dev/vality/magista/config/properties/TokenGenProperties.java
@@ -1,12 +1,12 @@
 package dev.vality.magista.config.properties;
 
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.annotation.Validated;
 
-import javax.validation.constraints.NotEmpty;
 
 @Getter
 @Setter
